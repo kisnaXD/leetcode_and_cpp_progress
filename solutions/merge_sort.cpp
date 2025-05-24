c7bf8116de64ae9b0ec3a3e162bed0a8bdccf6a2
kisnaXD <krishnagera93@gmail.com>
2025-05-24 17:26:34 +0530

    Merge sort done

---
+++ solutions/merge_sort.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Merge Sort
+
+void merge(vector <int> &arr, int low, int mid, int high) {
+    vector<int> temp;
+    int i = low;
+    int j = mid+1;
+    while(i<=mid && j<=high) {
+        if(arr[i] <= arr[j]) {
+            temp.push_back(arr[i]);
+            i+=1;
+        } else {
+            temp.push_back(arr[j]);
+            j+=1;
+        }
+    }
+    while(i<=mid) {
+        temp.push_back(arr[i]);
+        i+=1;
+    }
+    while(j<=high) {
+        temp.push_back(arr[j]);
+        j+=1;
+    }
+    for(int c = low; c<=high; c++) {
+        arr[c] = temp[c-low];
+    }
+}
+
+void mS(vector<int> &arr, int low, int high) {
+    if(low == high) return;
+    int mid = (low+high)/2;
+    mS(arr, low, mid);
+    mS(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+
+int main() {
+    vector<int> v;
+    v.push_back(3);
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(1);
+    mS(v, 0, 4);
+    for(int i=0; i<5; i++) {
+        cout << v[i] << endl;
+    }
+}
