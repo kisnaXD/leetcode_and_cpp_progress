16510f4b1bfcc8d5f98234cf4ed38f7418d0e868
kisnaXD <krishnagera93@gmail.com>
2025-05-25 18:05:28 +0530

    Rotate left by K places

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,17 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Removing Duplicates from a Sorted Array
+// Shift an array to the left by K places
 
-int rmDuplicates(vector <int> &arr, int N) {
-    int i = 0;
-    for(int j=1; j<N; j++) {
-        if(arr[j] != arr[i]) {
-            arr[i+1] = arr[j];
-            i+=1;
-        }
+void shiftLeftByOne(vector <int> &arr, int N) {
+    int temp = arr[0];
+    for(int i=1; i<N; i++) {
+        arr[i-1] = arr[i];
     }
-    return i+1;
+    arr[N-1] = temp;
+}
+
+void reverse(vector <int> &arr, int low, int high) {
+    int stop = (high+low)/2;
+    for(int i=low; i<=stop; i++) {
+        int temp = arr[i];
+        arr[i] = arr[high+low-i];
+        arr[high+low-i] = temp;
+    }
+}
+
+void shiftLeftByK(vector <int> &arr, int N, int K) {
+    K = K%N;
+    reverse(arr, 0, K-1);
+    reverse(arr, K, N-1);
+    reverse(arr, 0, N-1);
 }
 
 int main() {
@@ -21,12 +34,8 @@ int main() {
     v.push_back(2);
     v.push_back(3);
     v.push_back(4);
-    cout << "Array before removing duplicates" << endl;
-    for(auto it: v) {
-        cout << it << endl;
-    }
-    cout << "Number of duplicates : " << rmDuplicates(v, 5) << endl;
-    cout << "Array after removing duplicates" << endl;
+    shiftLeftByK(v, 5, 3);
+    cout << "Output" << endl;
     for(auto it: v) {
         cout << it << endl;
     }
