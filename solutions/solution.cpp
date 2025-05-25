cedfb38ccdd9cb3ac6575810db66691eaf636075
kisnaXD <krishnagera93@gmail.com>
2025-05-25 21:50:44 +0530

    Union of Two Sorted Arrays

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,44 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Move zeroes to end
+// Union of Two Sorted Arrays
 
-void moveZeroesToEnd(vector <int> &arr, int N) {
-    int j = -1;
-    for(int i=0; i<N; i++) {
-        if(arr[i] == 0) {
-            j = i;
-            break;
-        } else j = N;
+vector<int> unionOfTwoSortedArrays(vector <int> &arr1, int n1, vector <int> &arr2, int n2) {
+    vector <int> final;
+    int i=0, j=0;
+    while(i<n1 && j<n2) {
+        if(arr1[i] <= arr2[j]) {
+            if(final.size() == 0 || final.back() != arr1[i]) {
+                final.push_back(arr1[i]);
+            }
+            i+=1;
+        } else {
+            if(final.size() == 0 || final.back() != arr2[j]) {
+                final.push_back(arr2[j]);
+            }
+            j+=1;
+        }
     }
-    if(j==N) return;
-    int i = j+1;
-    while(i < N && j < N) {
-        if(arr[i] != 0) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j++;
-        } else i++;
+    while(i<n1) {
+        if(final.size() == 0 || final.back() != arr1[i]) {
+            final.push_back(arr1[i]);
+        }
+        i++;
     }
+    while(j<n2) {
+        if(final.size() == 0 || final.back() != arr2[j]) {
+            final.push_back(arr2[j]);
+        }
+        j++;
+    }
+    return final;
 }
 
 int main() {
     vector<int> v;
-    v.push_back(1);
     v.push_back(0);
+    v.push_back(1);
+    v.push_back(2);
     v.push_back(2);
     v.push_back(3);
-    v.push_back(2);
-    v.push_back(0);
-    v.push_back(0);
-    v.push_back(4);
-    v.push_back(5);
-    v.push_back(1);
-    moveZeroesToEnd(v, 10);
+    vector<int> v1;
+    v1.push_back(0);
+    v1.push_back(0);
+    v1.push_back(1);
+    v1.push_back(4);
+    v1.push_back(5);
+    vector<int> output = unionOfTwoSortedArrays(v, 5, v1, 5);
     cout << "Output" << endl;
-    for(auto it: v) {
+    for(auto it: output) {
         cout << it << endl;
     }
 }
