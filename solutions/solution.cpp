966489758aa4d681795554dae154f0057e37be74
kisnaXD <krishnagera93@gmail.com>
2025-06-01 01:36:37 +0530

    Three sum problem

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -3,61 +3,54 @@ using namespace std;
 
 // Majority Element - II 
 
-vector<int> majorityElementNBy3(vector<int> &arr, int N) {
-    vector<int> v;
-    int count1=0, count2=0, el1=INT_MIN, el2=INT_MIN;
+vector< vector < int > > threeSumProblem(vector<int> arr, int N, int K) {
+    vector< vector< int > > st;
     for(int i=0; i<N; i++) {
-        if(count1==0 && el2 != arr[i]) {
-            count1=1;
-            el1 = arr[i];
-        } else if (count2==0 && el1 != arr[i]) {
-            count2=1;
-            el2 = arr[i];
-        } else if (arr[i] == el1) {
-            count1++;
-        } else if(arr[i] == el2) {
-            count2++;
-        } else {
-            count1--;
-            count2--;
+        if(i>0 && arr[i] == arr[i-1]) continue;
+        int j = i+1, k=N-1;
+        
+        while(j<k) {
+            int sum = arr[i] + arr[j]+arr[k];
+            if(sum>K) {
+                k--;
+            } else if(sum<K) {
+                j++;
+            } else {
+                vector<int> v;
+                v.push_back(arr[i]);
+                v.push_back(arr[j]);
+                v.push_back(arr[k]);
+                st.push_back(v);
+                j++;
+                k--;
+                while(j < k && arr[j] == arr[j-1]) j++;
+                while(j < k && arr[k] == arr[k+1]) k--;
+            }
         }
     }
-    count1=0, count2=0;
-    for(int i=0; i<N; i++) {
-        if(el1 == arr[i]) count1++;
-        if(el2 == arr[i]) count2++;
-    }
-    int mini = (int)(N/3 + 1);
-    if(count1>=mini) {
-        v.push_back(el1);
-    }
-    if(count2>=mini) {
-        v.push_back(el2);
-    }
-    if(v.size() == 2) {
-        if(v[0] > v[1]) {
-            int temp = v[0];
-            v[0] = v[1];
-            v[1] = temp;
-        }
-    }
-    return v;
+    return st;
 }
 
 int main() {
     vector<int> v;
+    v.push_back(-2);
+    v.push_back(-2);
+    v.push_back(-2);
+    v.push_back(-1);
+    v.push_back(-1);
+    v.push_back(-1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(0);
     v.push_back(2);
     v.push_back(2);
-    v.push_back(1);
-    v.push_back(3);
-    v.push_back(1);
-    v.push_back(1);
-    v.push_back(3);
-    v.push_back(1);
-    v.push_back(1);
-    vector<int> ans = majorityElementNBy3(v, 9);
+    v.push_back(2);
+    vector< vector< int > > ans = threeSumProblem(v, 12, 0);
     for(auto it: ans) {
-        cout << it << endl;
+        for(auto itt: it) {
+            cout << itt << " ";
+        }
+        cout << endl;
     } 
     return 0;
 }
---
+++ solutions/threeSumProblem.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Majority Element - II 
+
+vector< vector < int > > threeSumProblem(vector<int> arr, int N, int K) {
+    vector< vector< int > > st;
+    for(int i=0; i<N; i++) {
+        if(i>0 && arr[i] == arr[i-1]) continue;
+        int j = i+1, k=N-1;
+        
+        while(j<k) {
+            int sum = arr[i] + arr[j]+arr[k];
+            if(sum>K) {
+                k--;
+            } else if(sum<K) {
+                j++;
+            } else {
+                vector<int> v;
+                v.push_back(arr[i]);
+                v.push_back(arr[j]);
+                v.push_back(arr[k]);
+                st.push_back(v);
+                j++;
+                k--;
+                while(j < k && arr[j] == arr[j-1]) j++;
+                while(j < k && arr[k] == arr[k+1]) k--;
+            }
+        }
+    }
+    return st;
+}
+
+int main() {
+    vector<int> v;
+    v.push_back(-2);
+    v.push_back(-2);
+    v.push_back(-2);
+    v.push_back(-1);
+    v.push_back(-1);
+    v.push_back(-1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(2);
+    v.push_back(2);
+    v.push_back(2);
+    vector< vector< int > > ans = threeSumProblem(v, 12, 0);
+    for(auto it: ans) {
+        for(auto itt: it) {
+            cout << itt << " ";
+        }
+        cout << endl;
+    } 
+    return 0;
+}
