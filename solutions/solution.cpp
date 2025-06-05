1758f392e2c623aefcb7557fa886fb3bfe3ff74a
kisnaXD <krishnagera93@gmail.com>
2025-06-06 01:35:55 +0530

    Finding Inversions

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,36 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Find Missing and Repeating Numbers
-
-pair<int,int> missingAndRepeating(vector<int> &arr, int N) {
-    int sum1 = N * (N+1) / 2;
-    int sum2 = N * (N+1) * (2*N+1) / 6;
-    int s1 = 0, s2 = 0;
-    for(int i = 0; i<N; i++) {
-        s1+=arr[i];
-        s2+=arr[i]*arr[i];
+// Counting Inversions
+int cnt = 0;
+void merge(vector <int> &arr, int low, int mid, int high) {
+    vector<int> temp;
+    int i = low;
+    int j = mid+1;
+    while(i<=mid && j<=high) {
+        if(arr[i] <= arr[j]) {
+            temp.push_back(arr[i]);
+            i+=1;
+        } else {
+            cnt += mid-i+1;
+            temp.push_back(arr[j]);
+            j+=1;
+        }
     }
-    int xMinusY = s1 - sum1;
-    int x2MinusY2 = s2 - sum2;
-    int xPlusY = x2MinusY2 / xMinusY;
-    int x = (xMinusY + xPlusY) / 2;
-    int y = (xPlusY - xMinusY) / 2;
-    pair <int, int> p;
-    p.first = x;
-    p.second = y;
-    return p;
+    while(i<=mid) {
+        temp.push_back(arr[i]);
+        i+=1;
+    }
+    while(j<=high) {
+        temp.push_back(arr[j]);
+        j+=1;
+    }
+    for(int c = low; c<=high; c++) {
+        arr[c] = temp[c-low];
+    }
+}
+
+void mS(vector<int> &arr, int low, int high) {
+    if(low == high) return;
+    int mid = (low+high)/2;
+    mS(arr, low, mid);
+    mS(arr, mid+1, high);
+    merge(arr, low, mid, high);
 }
 
 int main() {
     vector<int> v;
+    v.push_back(3);
     v.push_back(1);
     v.push_back(2);
-    v.push_back(3);
-    v.push_back(2);
-    v.push_back(5);
-    v.push_back(6);
-    pair<int, int> p = missingAndRepeating(v, 6);
-    cout << p.first << " " << p.second;
+    v.push_back(4);
+    v.push_back(1);
+    mS(v, 0, 4);
+    cout << cnt;
 }
-
