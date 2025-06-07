2c69a65c7dd44fd4fcc6700738148edd384d5167
kisnaXD <krishnagera93@gmail.com>
2025-06-08 02:34:09 +0530

    Search in rotated sorted array

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// First & Last Occurrences
+// Search in Rotated Sorted Array (without Duplicates)
 
-int firstOccurrence(vector<int> &arr, int N, int a) {
-    int low = 0, high = N-1, ans = -1;
-    while(low<=high) {
+int searchRotatedSorted1(vector<int> &arr, int N, int a) {
+    int low = 0;
+    int high = N-1;
+    while(low <= high) {
         int mid = (low+high)/2;
         if(arr[mid] == a) {
-            ans = mid;
-            high = mid - 1;
-        } else if(arr[mid] < a){
-            low = mid + 1;
-        } else high = mid - 1;
+            return mid;
+        } else if(arr[mid] < arr[high]) {
+            if(a<=arr[high] && a>=arr[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        } else {
+            if(a<=arr[mid] && a>=arr[low]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
     }
-    return ans;
+    return -1;
 }
-
 
 int main() {
     vector<int> v;
-    v.push_back(1);
-    v.push_back(1);
-    v.push_back(1);
-    v.push_back(2);
     v.push_back(2);
     v.push_back(3);
     v.push_back(4);
-    int firstOccurence3 = firstOccurrence(v, 7, 3);
-    cout << firstOccurence3;
+    v.push_back(1);
+    int rotatedthree = searchRotatedSorted1(v, 4, 3);
+    cout << rotatedthree;
 }
