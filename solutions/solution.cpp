dcaaab9c94f5d57ae4591635b3bbb9c86d50df39
kisnaXD <krishnagera93@gmail.com>
2025-06-07 17:05:44 +0530

    Search Insert Position

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Lower Bound & Upper Bound
+// Search Insert position
 
-int lowerBound(vector<int> &arr, int N, int a) {
+int searchInsert(vector<int> &arr, int N, int a) {
     int low = 0;
-    int ans = N-1;
+    int ans = N;
     int high = N-1;
     while(low<=high) {
         int mid = (low+high)/2;
@@ -23,36 +23,12 @@ int lowerBound(vector<int> &arr, int N, int a) {
     } 
     return ans;
 }
-
-int upperBound(vector<int> &arr, int N, int a) {
-    int low = 0;
-    int ans = 0;
-    int high = N-1;
-    while(low <= high) {
-        int mid = (low+high)/2;
-        if(arr[mid] == a) {
-            while(arr[mid] == arr[mid+1]) {
-                mid ++;
-            } 
-            return mid;
-        } else if (arr[mid] < a) {
-            ans = mid;
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
-    }
-    return ans;
-}
-
 int main() {
     vector<int> v;
     v.push_back(1);
     v.push_back(1);
     v.push_back(2);
-    v.push_back(3);
     v.push_back(4);
-    int lb = lowerBound(v, 5, 1);
-    int ub = upperBound(v, 5, 1);
-    cout << lb << " " << ub;
+    int find3 = searchInsert(v, 4, 3);
+    cout << find3;
 }
