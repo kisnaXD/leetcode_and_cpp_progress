4469d5c861372a7808baa5615cbfc5b84d7f5325
kisnaXD <krishnagera93@gmail.com>
2025-06-08 23:35:08 +0530

    Minimum in Rotated Sorted Array

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,30 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Search in Rotated Sorted Array (without Duplicates)
+// Minimum in Rotated Sorted Array
 
-int searchRotatedSorted1(vector<int> &arr, int N, int a) {
+int minimumInRotatedSorted(vector<int> &arr, int N) {
     int low = 0;
     int high = N-1;
-    while(low <= high) {
+    int ans = INT_MAX;
+    while(low<=high) {
         int mid = (low+high)/2;
-        if(arr[mid] == a) {
-            return mid;
-        } else if(arr[mid] < arr[high]) {
-            if(a<=arr[high] && a>=arr[mid]) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+        ans = ans < arr[mid] ? ans : arr[mid];
+        if(arr[mid] <= arr[high]) {
+            ans = ans < arr[mid] ? ans : arr[mid];
+            high = mid - 1;
         } else {
-            if(a<=arr[mid] && a>=arr[low]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+            ans = ans < arr[low] ? ans : arr[low];
+            low = mid + 1;
         }
     }
-    return -1;
+    return ans;
 }
 
 int main() {
@@ -33,6 +27,6 @@ int main() {
     v.push_back(3);
     v.push_back(4);
     v.push_back(1);
-    int rotatedthree = searchRotatedSorted1(v, 4, 3);
-    cout << rotatedthree;
+    int minimum = minimumInRotatedSorted(v, 4);
+    cout << minimum;
 }
