afd8bbd7752303a7b9072299d3744ceeb47538a0
kisnaXD <krishnagera93@gmail.com>
2025-06-15 01:22:28 +0530

    Median of Two Sorted Arrays & Minimise Maximum DIstance b/w 2 Gas Stations

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,48 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Book Allocation 
+// Find the median of two sorted arrays without merging them
 
-int paintSplit(vector<int> &arr, int N, int m, int s) {
-    int k = 1, cu = 0;
-    for(int i=0; i<N; i++) {
-        if(arr[i] + cu > m) { 
-            cu = arr[i];
-            k++;
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    int m = nums1.size();
+    int n = nums2.size();
+    int N = m+n;
+    int left = (m+n+1)/2;
+    if(m>n) {
+        return findMedianSortedArrays(nums2, nums1);
+    }
+    int low = 0, high = m;
+    while(low<=high) {
+        int mid = (low+high)/2;
+        int oMid = left - mid;
+        int l1 = INT_MIN, l2=INT_MIN;
+        int r1 = INT_MAX, r2=INT_MAX;
+        if(mid<m) r1 = nums1[mid];
+        if(oMid<n) r2 = nums2[oMid];
+        if(mid-1>=0) l1 = nums1[mid-1];
+        if(oMid-1>=0) l2 = nums2[oMid-1];
+        if(l1<=r2 && l2<=r1) {
+            if(N%2 != 0) {
+                return max(l1,l2);
+            } else {
+                return (double) (max(l1,l2) + min(r1,r2)) / 2;
+            }
+        } else if (l1>r2) {
+            high = mid - 1;
         } else {
-            cu+=arr[i];
+            low = mid + 1;
         }
     }
-    return k;
-}
-
-int paintersPartition(vector<int> &arr, int N, int s) {
-    int max=0,sum=arr[0];
-    for(int i=0; i<N; i++) {
-        if(arr[i] > max) {
-            max = arr[i];
-        }
-        sum+=arr[i];
-    }
-    while(max<=sum) {
-        int mid = (max+sum)/2;
-        int k = paintSplit(arr, N, mid, s);
-        if(k  > s) {
-            max = mid + 1;
-        } else {
-            sum = mid - 1;
-        }
-    }
-    return max;
+    return 0;
 }
 
 int main() {
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
-    v.push_back(6);
-    int pp = paintersPartition(v, 5, 3);
-    cout << pp;
+    vector<int> v1;
+    v1.push_back(7);
+    v1.push_back(12);
+    v1.push_back(14);
+    v1.push_back(15);
+
+    vector<int> v2;
+    v2.push_back(1);
+    v2.push_back(2);
+    v2.push_back(3);
+    v2.push_back(4);
+    v2.push_back(9);
+    v2.push_back(11);
+
+    double median2Arrays = findMedianSortedArrays(v1, v2);
+    cout << median2Arrays;
 }
