71385b4c0b17acb23dbdfdd2e2f4473e45c974bc
kisnaXD <krishnagera93@gmail.com>
2025-06-21 19:48:05 +0530

    Depth of Parantheses

--- solutions/Binary Search/searchInsert.cpp
+++ solutions/Binary Search/searchInsert.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 int searchInsert(vector<int> &arr, int N, int a) {
     int low = 0;
-    int ans = -1;
+    int ans = N;
     int high = N-1;
     while(low<=high) {
         int mid = (low+high)/2;
         if(arr[mid] == a) {
-            return arr[mid];
+            return mid;
         } else if (arr[mid] < a) {
-            ans = arr[mid];
             low = mid + 1;
         } else {
+            ans = mid;
             high = mid-1;
         }
     } 
@@ -22,10 +22,7 @@ int searchInsert(vector<int> &arr, int N, int a) {
 }
 int main() {
     vector<int> v;
-    v.push_back(1);
-    v.push_back(1);
-    v.push_back(2);
     v.push_back(4);
-    int find3 = searchInsert(v, 4, 3);
+    int find3 = searchInsert(v, 1, 5);
     cout << find3;
 }
